7eace37a748cb284bfce4e2f53f0649c7ca6b74a
yalcin-mrl <94745103+yalcin-mrl@users.noreply.github.com>
2022-05-31 10:25:46 +0300

    Initial commit

---
+++ src/PolindromikKelime.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PolindromikKelime {
+    static boolean isPolindrome(String str){
+        int i = 0, j = str.length()-1;
+        while(i<j){
+            if(str.charAt(i) != str.charAt(j))
+                return false;
+                i++;
+                j--;
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println(isPolindrome("abba"));
+    }
+}
